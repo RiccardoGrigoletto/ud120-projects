3bdc881503dd173208fca4267c90654e4bcaa4ec
Riccardo Grigoletto <riccardogrigo@gmail.com>
2018-04-27 09:35:08 +0200

    implement evalauation project, run it using python 2

--- evaluation/evaluate_poi_identifier.py
+++ evaluation/evaluate_poi_identifier.py
@@ -1,6 +1,11 @@
-#!/usr/bin/python
+#!/usr/bin/python2
 
 
+####################################
+####################################
+#           USE PYTHON 2           #
+####################################
+####################################
 """
     Starter code for the evaluation mini-project.
     Start by copying your trained/tested POI identifier from
@@ -28,4 +33,41 @@ labels, features = targetFeatureSplit(data)
 
 ### your code goes here 
 
+from sklearn.tree import DecisionTreeClassifier
 
+dtc = DecisionTreeClassifier()
+clf = dtc.fit(features,labels)
+pred = clf.predict(features)
+
+from sklearn.metrics import accuracy_score
+
+print("accuracy using the entire dataset for both training and testing: %.4f (very overfitted)"%accuracy_score(labels,pred))
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(features,labels, test_size=0.3, random_state=42)
+
+clf = dtc.fit(X_train,y_train)
+pred = clf.predict(X_test)
+print ("accuracy using cross validation with 30/100 for testing: %.4f "%accuracy_score(pred,y_test))
+
+from collections import Counter
+print ("Test data labels: " + str(Counter(y_test)))
+
+from sklearn.metrics import confusion_matrix
+from prettytable import PrettyTable
+print ("Confusion matrix using cross validation with 30/100 for testing:")
+pt = PrettyTable()
+tn, fp, fn, tp = confusion_matrix(y_test,pred).ravel()
+pt.add_row(["True -> \n Predicted \\|/","Poi","!Poi"])
+pt.add_row(["Poi",tp,fp])
+pt.add_row(["!Poi",fn,tn])
+print (pt)
+
+print ("POI precision: %.4f"%(tp/(tp+fp)))
+print ("POI recall: %.4f"%(tp/(tp+fn)))
+print("Predictions vs True:")
+pt = PrettyTable()
+pt.add_column("Predictions", pred)
+pt.add_column("True",y_test)
+#print (pt)
